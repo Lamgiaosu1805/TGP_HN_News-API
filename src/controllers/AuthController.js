bc7c420c53eac3f21495219cda16429c862aad81
LamGS <nghiemkhaclam185.mta@gmail.com>
2024-03-28 08:28:00 +0700

    add jwt

---
+++ src/controllers/AuthController.js
@@ -0,0 +1,12 @@
+
+const AuthController = {
+    signUp: (req, res, next) => {
+        try {
+            
+        } catch (error) {
+            
+        }
+    }
+}
+
+module.exports = AuthController;
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ const testRouter = require('./test')
 const api_ver = "/api/v1"
 const route = (app) => {
     app.use(`${api_ver}/test`, testRouter)
+    app.use(`${api_ver}/auth`, testRouter)
 }
 
 module.exports = route;
