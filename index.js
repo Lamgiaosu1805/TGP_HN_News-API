f68d078e2171cae5e95351585308081bae3b7e1e
LâmGS <nghiemkhaclam185.mta@gmail.com>
2024-04-10 16:01:17 +0700

    fix cors

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const port = 3000
 const route = require('./src/routes')
 const morgan = require('morgan')
 const db = require('./src/config/connectdb')
+const cors=require("cors");
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
 
 //Load .env
 require('dotenv').config();
@@ -14,6 +20,7 @@ app.use(express.json())
 app.use(express.urlencoded({
   extended: true
 }))
+app.use(cors(corsOptions))
 
 //connect DB
 db.connect()
