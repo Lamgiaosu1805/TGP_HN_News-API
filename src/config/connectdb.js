2077e8f43cf23f5e7662c06e69bca0beec9347b5
Lamgs <nghiemkhaclam185.mta@gmail.com>
2024-05-01 00:26:05 +0700

    change ENV

--- src/config/connectdb.js
+++ src/config/connectdb.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const connect = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URL);
+        await mongoose.connect(process.env.MONGODB_URI);
         console.log("DB connected");
     } catch (error) {
         console.log("Connect failure", error);
