2196161c2e43a7be74ad7fd908ebaecc4de8407f
Lamgs <nghiemkhaclam185.mta@gmail.com>
2024-04-30 19:21:38 +0700

    improve

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,9 @@ const linhMucRouter = require('./linhMucDoan')
 
 const api_ver = "/api/v1"
 const route = (app) => {
+    app.use(`/`, (req, res, next) => {
+        res.send("Hello world")
+    });
     app.use(`${api_ver}/auth`, authRouter);
     app.use(`${api_ver}/post`, postRouter);
     app.use(`${api_ver}/lichCongGiao`, lichRouter);
