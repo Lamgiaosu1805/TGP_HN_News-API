8bd8e46e0ec00e419cb9b6e19b11a97dc4305023
Lamgs <nghiemkhaclam185.mta@gmail.com>
2024-05-14 14:22:56 +0700

    get list giao xu & search

--- src/routes/giaoXu.js
+++ src/routes/giaoXu.js
@@ -2,6 +2,8 @@ const express = require('express');
 const GiaoXuController = require('../controllers/GiaoXuController');
 const router = express.Router()
 
-router.get('/save', GiaoXuController.getGiaoXu);
+// router.get('/save', GiaoXuController.getGiaoXu);
+
+router.post('/search', GiaoXuController.searchGiaoXu);
 
 module.exports = router;
--- src/routes/index.js
+++ src/routes/index.js
@@ -7,15 +7,15 @@ const giaoXuRouter = require('./giaoXu')
 
 const api_ver = "/api/v1"
 const route = (app) => {
+    // app.use(`/`, (req, res, next) => {
+    //     res.send("Hello world")
+    // });
     app.use(`${api_ver}/auth`, authRouter);
     app.use(`${api_ver}/post`, postRouter);
     app.use(`${api_ver}/lichCongGiao`, lichRouter);
     app.use(`${api_ver}/linhMucDoan`, linhMucRouter);
     app.use(`${api_ver}/giaoHat`, giaoHatRouter);
     app.use(`${api_ver}/giaoXu`, giaoXuRouter);
-    app.use(`/`, (req, res, next) => {
-        res.send("Hello world")
-    });
 }
 
 module.exports = route;
